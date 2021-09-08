2192b2ce26d23a5c39ee2f5ea05507c2c1a7a20b
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-09-08 23:47:38 +0900

    UPADTE piece

--- src/Piece.ts
+++ src/Piece.ts
@@ -1,3 +1,29 @@
+import { Cell, Position } from './Board';
+import { PlayerType } from './Player';
+
+export class MoveResult {
+  constructor(private killedPiece: Piece){
+    getKilled(){
+      return this.killedPiece;
+    }
+  }
+}
+
 export interface Piece {
-  render();
+  ownerType: PlayerType;
+  currentPosition: Position
+  move(from: Cell, to: Cell): MoveResult;
+  render(): string;
+}
+
+abstract class DefaultPiece implements Piece{
+  constructor(public readonly ownerType: PlayerType, public currentPosition){
+
+  }
+  move(from: Cell, to: Cell): MoveResult{
+    throw new Error('Method not implemented.');
+  }
+  render(): string{
+    throw new Error('Method not implemented.')
+  }
 }
