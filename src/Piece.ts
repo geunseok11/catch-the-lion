c2ff84e9b621d3b7e0e5511dba7dc2405b41fca7
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-09-09 22:17:45 +0900

    UPDATE piece

--- src/Piece.ts
+++ src/Piece.ts
@@ -1,5 +1,11 @@
 import { Cell, Position } from './Board';
 import { PlayerType } from './Player';
+import loinImage from './images/lion.png';
+import chickenImage from './images/chicken.png';
+import griffImage from './images/griff.png';
+import elophantImage from './images/elophant.png';
+
+console.log(chickenImage)
 
 export class MoveResult {
   constructor(private killedPiece: Piece){
@@ -21,9 +27,71 @@ abstract class DefaultPiece implements Piece{
 
   }
   move(from: Cell, to: Cell): MoveResult{
-    throw new Error('Method not implemented.');
+    if(!this.canMove(to.position)){
+      throw new Error('Method not implemented.');
+    }
+    
+    const moveResult = new MoveResult((to.getPiece() != null) ? to.getPiece() : null)
+    to.put(this)
+    from.put(null)
+    this.currentPosition = to.position
+    return moveResult
   }
-  render(): string{
-    throw new Error('Method not implemented.')
+  
+  abstract canMove(position: Position):boolean
+  abstract render()
+}
+
+export class Lion extends DefaultPiece {
+  canMove(pos: Position) {
+    const canMove = (pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col)
+      || (pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col)
+      || (pos.col === this.currentPosition.col + 1 && pos.row === this.currentPosition.row)
+      || (pos.col === this.currentPosition.col - 1 && pos.row === this.currentPosition.row)
+      || (pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col + 1)
+      || (pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col - 1)
+      || (pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col + 1)
+      || (pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col - 1);
+    return canMove;
+  }
+
+  render(): string {
+    return `<img class="piece ${this.ownerType}" src="${loinImage}" width="90%" height="90%"/>`;
+  }
+}
+
+export class Elephant extends DefaultPiece {
+  canMove(pos: Position) {
+    return (pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col + 1)
+      || (pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col - 1)
+      || (pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col + 1)
+      || (pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col - 1);
+  }
+
+  render(): string {
+    return `<img class="piece ${this.ownerType}" src="${elophantImage}" width="90%" height="90%"/>`;
+  }
+}
+
+export class Griff extends DefaultPiece {
+  canMove(pos: Position) {
+    return (pos.row === this.currentPosition.row + 1 && pos.col === this.currentPosition.col)
+      || (pos.row === this.currentPosition.row - 1 && pos.col === this.currentPosition.col)
+      || (pos.col === this.currentPosition.col + 1 && pos.row === this.currentPosition.row)
+      || (pos.col === this.currentPosition.col - 1 && pos.row === this.currentPosition.row);
+  }
+
+  render(): string {
+    return `<img class="piece ${this.ownerType}" src="${griffImage}" width="90%" height="90%"/>`;
+  }
+}
+
+export class Chick extends DefaultPiece {
+  canMove(pos: Position) {
+    return this.currentPosition.row + ((this.ownerType == PlayerType.UPPER) ? +1 : -1) === pos.row;
+  }
+
+  render(): string {
+    return `<img class="piece ${this.ownerType}" src="${chickenImage}" width="90%" height="90%"/>`;
   }
 }
